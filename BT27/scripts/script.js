05719a47e3b0139150c7e30592233f8c910f9d06
hdtruong231 <ductruong231@gmail.com>
2024-10-24 09:30:25 +0700

    BT27

---
+++ BT27/scripts/script.js
@@ -0,0 +1,59 @@
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+let outputText = '';
+
+btn.addEventListener('click', () => {
+    outputText = '';
+    // Tạo một Promise mới mỗi khi nhấn nút
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let random = Math.floor(Math.random() * 10);
+            if (random % 2 === 0) {
+                resolve('Number: ' + random + ' Success at step 1');
+            } else {
+                reject('Number: ' + random + ' Failure at step 1');
+            }
+        }, 200);
+    })
+    .then((message) => {
+        console.log(message);
+        outputText += `<p class="cardtext text-xl font-bold text-center">${message}</p>`;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let random = Math.floor(Math.random() * 10);
+                if (random % 2 === 0) {
+                    resolve('Number: ' + random + ' Success at step 2');
+                } else {
+                    reject('Number: ' + random + ' Failure at step 2');
+                }
+            }, 200);
+        });
+    })
+    .then((message) => {
+        console.log(message);
+        outputText += `<p class="cardtext text-xl font-bold text-center">${message}</p>`;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let random = Math.floor(Math.random() * 10);
+                if (random % 2 === 0) {
+                    resolve('Number: ' + random + ' Success at step 3');
+                } else {
+                    reject('Number: ' + random + ' Failure at step 3');
+                }
+            }, 200);
+        });
+    })
+    .then((message) => {
+        console.log(message);
+        outputText += `<p class="cardtext text-xl font-bold text-center">${message}</p>`;
+    })
+    .catch((error) => {
+        console.error(error);
+        outputText += `<p class="cardtext text-xl font-bold text-center">${error}</p>`;
+    })
+    .finally(() => {
+        output.innerHTML = outputText;
+         
+    });
+    
+});
